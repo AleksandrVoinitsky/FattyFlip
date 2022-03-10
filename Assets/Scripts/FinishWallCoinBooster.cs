c353280ab4dc86019ae2983e1efff0110d767fbe
Timur Shakirov <pirozhok.studio@mail.ru>
2022-03-10 15:11:25 +0400

    Fixes

--- Assets/Scripts/FinishWallCoinBooster.cs
+++ Assets/Scripts/FinishWallCoinBooster.cs
@@ -7,11 +7,14 @@ public class FinishWallCoinBooster : MonoBehaviour
 {
     [SerializeField] int Boost;
     [SerializeField] GameObject Text;
+    [SerializeField] private GameObject confetti;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Player")
         {
             Destroy(Text);
+            Instantiate(confetti, other.transform.position, Quaternion.identity);
             LevelManager manager = FindObjectOfType<LevelManager>();
 
             manager.AddScore(manager.FinishScore * Boost);
